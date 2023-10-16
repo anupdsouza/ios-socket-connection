874ea0315b62e384dc33a3f17b0ec5a809dd82b7
Anup DSouza <anup.dsouza@gmail.com>
2023-10-16 14:01:54 +0530

    Update Starscream dependency tp 4.0.4
    Fixes web socket delegate error in SocketEngine
    SocketIO uses Starscream 4.0.6, manually add Starscream package withe exact version 4.0.4 to overwrite the dependency

--- SocketConnection/SocketConnection/ContentView.swift
+++ SocketConnection/SocketConnection/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var socketService = SocketService()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            HStack {
+                Button("Connect") {
+                    socketService.connectToServer()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                
+                Button("Disconnect") {
+                    socketService.disconnectFromServer()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            }
         }
         .padding()
     }
