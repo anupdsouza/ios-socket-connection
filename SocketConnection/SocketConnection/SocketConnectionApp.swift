582a95237a753b66a6e210a65a3cb7ec974e626f
Anup DSouza <anup.dsouza@gmail.com>
2023-10-16 12:52:28 +0530

    Add initial files

---
+++ SocketConnection/SocketConnection/SocketConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketConnectionApp.swift
+//  SocketConnection
+//
+//  Created by Anup D'Souza on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SocketConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
