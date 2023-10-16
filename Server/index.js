b595c97b6562913bae71a08dacfc79486447b1b4
Anup DSouza <anup.dsouza@gmail.com>
2023-10-16 14:14:08 +0530

    Add minimal server code

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+// User connections
+io.on('connection', (socket) => {
+  console.log(`=> A User connected`);
+
+  // User disconnections
+  socket.on('disconnect', () => {
+    console.log(`=> A User disconnected`);
+    socket.emit('userDisconnected', socket.id);
+  });
+});
